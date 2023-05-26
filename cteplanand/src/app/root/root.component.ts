7ae8d5d08ed12206246b620d2d85a2d8125b0f24
dotnet-code <@dotnet-code>
2023-05-26 11:31:24 +0530

    commit from code202305263124

---
+++ cteplanand/src/app/root/root.component.ts
@@ -0,0 +1,18 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <div>
+      <h2>Configuration of Github</h2>
+      <div>
+        {{error}}
+      </div>
+    </div>
+  `,
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Github Configuration';
+  error = `'NoneType' object has no attribute 'replace'`;
+}
